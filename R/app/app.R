88f3634d1dbde242aa2a782f2a34d422001cfaf0
Isaac Cardello <isaaccardello1221@gmail.com>
2022-11-08 17:01:02 -0500

    Clear Occurences panel

--- R/app/app.R
+++ R/app/app.R
@@ -13,12 +13,6 @@ navbar <- function() {
 
 occurencesPanel <- function() {
     tabPanel(title = "Occurences",
-        tags$head(tags$link(rel = "stylesheet", href="https://unpkg.com/leaflet@1.9.1/dist/leaflet.css", integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=", crossorigin="")),
-        tags$head(tags$script(src = "https://unpkg.com/leaflet@1.9.1/dist/leaflet.js", integrity="sha256-NDI0K41gVbWqfkkaHj15IzU7PtMoelkzyKp8TOaFQ3s=", crossorigin="")),
-        tags$head(tags$link(rel = "stylesheet", href = "https://unpkg.com/@geoman-io/leaflet-geoman-free@latest/dist/leaflet-geoman.css")),
-        tags$head(tags$script(src = "https://unpkg.com/@geoman-io/leaflet-geoman-free@latest/dist/leaflet-geoman.min.js")),
-        tags$div(id = "map", style = "height: 400px;"),
-        tags$script(src = "map.js")
     )
 }
 
